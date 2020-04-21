703f6a113383419699ea3301a9ad71c8c1fe2eaf
kresimir <kresomaric66@gmail.com>
2020-04-21 16:53:32 +0100

    commit

--- bin2dec/app.js
+++ bin2dec/app.js
@@ -1,12 +1,26 @@
 const form = document.querySelector('#form');
 const input = document.querySelector('input[type="text"]');
 const output = document.querySelector('.output');
+let par = document.createElement('p');
 
 
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     const value = document.querySelector('input[type="text"]').value;
-    output.textContent = parseInt(value, 2);
+    let isNum = /^\d+$/.test(value);
+    let isBinary = /^[01]+$/.test(value);
+    if (isNum && isBinary) {
+        output.textContent = parseInt(value, 2);
+        document.querySelector('.wrapper').classList.remove('warning');
+        par.remove();
+    } else {
+        document.querySelector('.wrapper').classList.add('warning');
+        form.appendChild(par);
+        par.textContent = 'Please enter binary number.';
+        par.classList.add('warning-par');
+    }
     input.value = '';
 });
+
+// par.remove();
