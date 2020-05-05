02958945e1650ef2b828276a3f3959c555cd1e09
kresimir <kresomaric66@gmail.com>
2020-05-05 08:43:49 +0100

    Lights now change colors randomly

--- 04-christmass-lights/app.js
+++ 04-christmass-lights/app.js
@@ -1,25 +1,38 @@
-lights = document.querySelectorAll('.light');
-
+const lights = document.querySelectorAll('.light');
+const switchBtn = document.querySelector('.switch');
+slider = document.querySelector('input');
 
 let lightsArray = Array.from(lights);
 
 const colors = ['#D32F2F', '#C2185B', '#7B1FA2', '#512DA8', '#303F9F', '#1976D2', '#0288D1', '#00796B', '#AFB42B', '#FFA000', '#E64A19'];
-const randomColor = Math.floor(Math.random() * colors.length);
-const randomLight = Math.floor(Math.random() * lightsArray.length);
-console.log(randomLight);
-
-function lightsOn(light) {
-        light.style.background = colors[randomNumber];
-        light.style.boxShadow = `0 5px 4px 8px ${colors[randomNumber]}50`;
-};
 
 
+    function lightsOn(light) {
+        let intervalTime = slider.value;
 
-    lightsArray.forEach((light, index) => {
-        console.log(index);
-        console.log(lightsArray);
-        lightsArray[index].style.background = colors[randomColor];
-        lightsArray[index].style.boxShadow = `0 5px 4px 8px ${colors[randomColor]}50`;
+        slider.addEventListener('change', (e) => {
+            e.preventDefault();
+            intervalTime = e.target.value;
+            console.log(`Change: ${intervalTime}`);
+        });
+
+
+        setInterval(() => {
+            let currentColor = light.style.background = colors[Math.floor(Math.random() * colors.length)];
+            light.style.boxShadow = `0 5px 4px 8px ${currentColor}50`;
+            // console.log(currentColor);
+            // ! How to update interval time ?
+        }, intervalTime);
+        console.log(intervalTime);
+    }
+
+    
+    // ! implement the switch lights feature
+    switchBtn.addEventListener('click', (light) => {
+        switchBtn.classList.toggle('switchOn');
     });
 
-
+    
+    lightsArray.forEach((light) => {
+        lightsOn(light);
+    });
