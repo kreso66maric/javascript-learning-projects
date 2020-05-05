a3ffee7779cc3c201688f24b7e658a084c71a76c
kresimir <kresomaric66@gmail.com>
2020-05-05 13:53:40 +0100

    some styles and functionality added

--- 04-christmass-lights/app.js
+++ 04-christmass-lights/app.js
@@ -1,38 +1,58 @@
 const lights = document.querySelectorAll('.light');
 const switchBtn = document.querySelector('.switch');
-slider = document.querySelector('input');
+const slider = document.querySelector('input');
 
 let lightsArray = Array.from(lights);
 
 const colors = ['#D32F2F', '#C2185B', '#7B1FA2', '#512DA8', '#303F9F', '#1976D2', '#0288D1', '#00796B', '#AFB42B', '#FFA000', '#E64A19'];
 
+let interval;
+let intervalTime;
 
-    function lightsOn(light) {
-        let intervalTime = slider.value;
+// Handles time interval for lights
+function handleInterval(light) {
+    interval = setInterval(() => {
+        currentColor = light.style.background = colors[Math.floor(Math.random() * colors.length)];
+        light.style.boxShadow = `0 5px 4px 8px ${currentColor}50`;
 
-        slider.addEventListener('change', (e) => {
-            e.preventDefault();
-            intervalTime = e.target.value;
-            console.log(`Change: ${intervalTime}`);
-        });
+        // ! How to update interval time ?
+    }, 1000);
+}
 
+// Handles slider for changing interval times
+function handleSlider() {
 
-        setInterval(() => {
-            let currentColor = light.style.background = colors[Math.floor(Math.random() * colors.length)];
-            light.style.boxShadow = `0 5px 4px 8px ${currentColor}50`;
-            // console.log(currentColor);
-            // ! How to update interval time ?
-        }, intervalTime);
-        console.log(intervalTime);
-    }
+    slider.addEventListener('change', (e) => {
+        intervalTime = parseInt(e.target.value);
+        console.log(`Change: ${intervalTime}`);
+    });
 
-    
+    slider.addEventListener('mouseover', (e) => {
+        intervalTime = parseInt(e.target.value);
+        console.log(`Change: ${intervalTime}`);
+    });
+}
+
+// Handles turning the lights on
+function lightsOn() {
+    lightsArray.forEach((light) => {
+        handleInterval(light);
+    });
+}
+
+function LightsOff() {
     // ! implement the switch lights feature
-    switchBtn.addEventListener('click', (light) => {
+    switchBtn.addEventListener('click', () => {
+
         switchBtn.classList.toggle('switchOn');
     });
+}
 
-    
-    lightsArray.forEach((light) => {
-        lightsOn(light);
-    });
+
+    // Main function
+    function init() {
+        lightsOn();
+        handleSlider();
+    }
+
+    init();
