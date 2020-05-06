1afbb734dda4afb162f20cc455dfb8685f38a56c
kresimir <kresomaric66@gmail.com>
2020-05-06 13:40:48 +0100

    project finnished

--- 04-christmass-lights/app.js
+++ 04-christmass-lights/app.js
@@ -6,36 +6,21 @@ let lightsArray = Array.from(lights);
 
 const colors = ['#D32F2F', '#C2185B', '#7B1FA2', '#512DA8', '#303F9F', '#1976D2', '#0288D1', '#00796B', '#AFB42B', '#FFA000', '#E64A19'];
 
-
-let intervalTime;
 let interval;
+let intervalTime = 500;
 
-
-
-
-// Handles slider for changing interval times
-function handleSlider() {
-
-    slider.addEventListener('change', (e) => {
-        intervalTime = parseInt(e.target.value);
-        console.log(`Change: ${intervalTime}`);
-    });
-
-    slider.addEventListener('mouseover', (e) => {
-        intervalTime = parseInt(e.target.value);
-        console.log(`Change: ${intervalTime}`);
-    });
-}
 
 // Handles turning the lights on
 function lightsOn() {
-    interval = setInterval(lightsOn, 1000);
-    lightsArray.forEach((light) => {
-        currentColor = light.style.background = colors[Math.floor(Math.random() * colors.length)];
-        light.style.boxShadow = `0 5px 4px 8px ${currentColor}50`;
-    });
+    interval = setInterval(() => {
+        lightsArray.forEach((light) => {
+            currentColor = light.style.background = colors[Math.floor(Math.random() * colors.length)];
+            light.style.boxShadow = `0 5px 4px 8px ${currentColor}50`;
+        });
+    }, intervalTime);
 }
 
+// Handles turning the lights off
 function lightsOff() {
     clearInterval(interval);
     lightsArray.forEach(light => {
@@ -44,11 +29,11 @@ function lightsOff() {
     });
 };
 
+// Handles the switching button
 function handlelightSwitch() {
     switchBtn.addEventListener('click', () => {
 
         switchBtn.classList.toggle('switchOn');
-        console.log(switchBtn.dataset.switch);
 
         if (switchBtn.dataset.switch === 'on') {
             switchBtn.setAttribute('data-switch', 'off');
@@ -65,7 +50,6 @@ function handlelightSwitch() {
     // Main function
     function init() {
         handlelightSwitch();
-        handleSlider();
     }
 
     init();
