f8e0947ffb729dbc88e897e5d2967cf17ffec90c
kresimir <kresomaric66@gmail.com>
2020-05-11 20:26:33 +0100

    Made some progres on the tic tac toe game

--- 05-tic-tac-toe/app.js
+++ 05-tic-tac-toe/app.js
@@ -1,9 +1,33 @@
 const x = '<div class="x"></div>';
 const o = '<div class="o"></div>';
-const block = document.querySelectorAll('.block');
+const blocks = document.querySelectorAll('.block');
+
+let playerTurn = true;
+
+const playerOne = {
+    
+}
+
+const allBlocks = blocks.forEach(block => block.addEventListener('click', handleClick, { once: true }));
+
+function handleClick(e) {
+    const block = e.target;
+    const currentClass = playerTurn ? x : o;
+    // place mark
+    placeMark(block, currentClass)
+    // check for win
+    // check for draw
+    // switch turns
+    playerTurn = !playerTurn;
+};
+
+function placeMark(block, currentClass) {
+    block.innerHTML = currentClass;
+    console.log(block.id, currentClass);
+}
+
+function handleWin() {
+}
 
 
-block.forEach(block => block.addEventListener('click', (e) => {
-    console.log(e.target.id);
-    block.innerHTML = x;
-}));
+
